57360ea99acd0ce81b025c6518eb8e67b57c2c55
José F. Romaniello <jfromaniello@gmail.com>
2025-11-04 16:11:06 -0300

    minor

--- app/winds/page.tsx
+++ app/winds/page.tsx
@@ -1,5 +1,27 @@
 import { redirect } from 'next/navigation'
 
-export default function WindsRedirect() {
-  redirect('/course')
+interface WindsRedirectProps {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}
+
+export default async function WindsRedirect({ searchParams }: WindsRedirectProps) {
+  const params = await searchParams;
+  const queryString = new URLSearchParams();
+
+  // Preserve all query parameters
+  Object.entries(params).forEach(([key, value]) => {
+    if (value !== undefined) {
+      if (Array.isArray(value)) {
+        value.forEach(v => queryString.append(key, v));
+      } else {
+        queryString.set(key, value);
+      }
+    }
+  });
+
+  const redirectUrl = queryString.toString()
+    ? `/course?${queryString.toString()}`
+    : '/course';
+
+  redirect(redirectUrl);
 }
