d2059e67cfcfa972c63f1276ae6bb3d4bb7f956a
José F. Romaniello <jfromaniello@gmail.com>
2025-11-04 14:27:07 -0300

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,18 @@ export const metadata: Metadata = {
   ],
   authors: [{ name: "José", url: "https://twitter.com/jfroma" }],
   creator: "José",
+  manifest: "/site.webmanifest",
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/favicon.svg", type: "image/svg+xml" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    ],
+  },
   robots: {
     index: true,
     follow: true,
